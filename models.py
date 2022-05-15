cbac9883383d25c0f0b8ecc9edbe74ba1ac691ff
gitavk <3apa3ka3@gmail.com>
2022-05-15 12:22:40 +0200

    cosmic Batch model.

--- models.py
+++ models.py
@@ -1,33 +1,38 @@
 from dataclasses import dataclass
 from datetime import date
-from typing import Optional
+from typing import Optional, Set
 
 
 @dataclass(frozen=True)
 class OrderLine:
-    title: str
+    orderid: str
     sku: str
     qty: int
 
 
 class Batch:
-    def __init__(self, ref: str, sku: str, qty: int, eta: Optional[date]) -> None:
-        self.ref: str = ref
-        self.sku: str = sku
-        self.available_quantity: int = qty
-        self.qty: int = qty
-        self.eta: Optional[date] = eta
+    def __init__(self, ref: str, sku: str, qty: int, eta: Optional[date]):
+        self.reference = ref
+        self.sku = sku
+        self.eta = eta
+        self._purchased_quantity = qty
+        self._allocations: Set[OrderLine] = set()
 
-    def allocate(self, line: OrderLine) -> None:
+    def allocate(self, line: OrderLine):
         if self.can_allocate(line):
-            self.available_quantity -= line.qty
+            self._allocations.add(line)
+
+    def deallocate(self, line: OrderLine):
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
 
     def can_allocate(self, line: OrderLine) -> bool:
-        is_right_sku: bool = self.sku == line.sku
-        enough_qty: bool = self.available_quantity >= line.qty
-        return is_right_sku and enough_qty
-
-    def deallocate(self, line: OrderLine) -> None:
-        if self.available_quantity + line.qty <= self.qty:
-            self.available_quantity += line.qty
-        
+        return self.sku == line.sku and self.available_quantity >= line.qty
